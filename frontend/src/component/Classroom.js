e8fcc655a4691a2cc27c4f4d4fa5ed311cc0824e
DeeprajB <deeprajb012@gmail.com>
2021-10-30 03:40:54 +0530

    Added UI for Course Page

--- frontend/src/component/Classroom.js
+++ frontend/src/component/Classroom.js
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router";
-import { Card, Button } from "antd";
+import { Skeleton, Card, Button } from "antd";
 import Select from "react-select";
+import { Row, Col } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+
 
 import { updateCourseNames, setCourseResources } from "../store/courseData";
 
+const { Meta } = Card;
 const Classroom = () => {
   const [courses, setCourses] = useState([]);
   const [courseResources, setCoursesResources] = useState({});
@@ -63,8 +67,7 @@ const Classroom = () => {
   };
 
   return (
-    <>
-      {courses.length === 0 && <h1>GLOBAL LOADING</h1>}
+    <div style={{ maxWidth: '60%', marginLeft:"24rem"}}>
       <Select
         isLoading={courses.length === 0}
         value={selectedCourse}
@@ -74,25 +77,72 @@ const Classroom = () => {
         }))}
         onChange={handleSelectChange}
       />
-      <div>
-        {Array.isArray(courseResources[selectedCourse.value]) ? (
+      <br />
+      <h1 style={{textAlign: 'center'}}>Lectures for the Course:</h1>
+      <div style={{marginTop: '2rem', marginLeft: '8.5%'}}>
+      
+      <Row gutter={[24, 16]}>
+                {Array.isArray(courseResources[selectedCourse.value]) ? (
           courseResources[selectedCourse.value].map((element) => (
-            <Card key={element.id}>
-              <Button
+            <Col span={8}>
+            <Card key={element.id} hoverable
+            style={{ width: 240, marginBottom: "2rem" }}
+            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+            onClick={() => redirectLecture(element.id, element.title)}>
+              
+              {/* <Button
                 type="primary"
                 onClick={() => redirectLecture(element.id, element.title)}
               >
                 GO TO LECTURE
-              </Button>
+              </Button> */}
 
-              <pre>{JSON.stringify(element, null, 2)}</pre>
+              {/* <pre>{JSON.stringify(element.title, null, 2)}</pre> */}
+              {element.title}
             </Card>
+            </Col>
           ))
         ) : (
-          <h1>LOADING</h1>
+          <>
+          <Col span={8}>
+            <Card
+          style={{ width: 300, marginTop: 16 }}>
+          <Skeleton avatar active>
+            <Meta
+              title="Card title"
+              description="This is the description"
+            />
+          </Skeleton>
+        </Card>
+            </Col>
+            <Col span={8}>
+            <Card
+          style={{ width: 300, marginTop: 16 }}>
+          <Skeleton avatar active>
+            <Meta
+              title="Card title"
+              description="This is the description"
+            />
+          </Skeleton>
+        </Card>
+            </Col>
+            <Col span={8}>
+            <Card
+          style={{ width: 300, marginTop: 16 }}>
+          <Skeleton avatar active>
+            <Meta
+              title="Card title"
+              description="This is the description"
+            />
+          </Skeleton>
+        </Card>
+            </Col>
+            </>
         )}
+        </Row>
+        
       </div>
-    </>
+    </div>
   );
 };
 
