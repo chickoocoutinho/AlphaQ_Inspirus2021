df5cf8a7d721f4dad0bb82888a66e77c3428f22a
Benito <benito.coutinho@xenerx.com>
2021-10-29 20:09:05 +0530

    passed lectureID to editor

--- frontend/src/Pages/Editor.js
+++ frontend/src/Pages/Editor.js
@@ -1,41 +1,40 @@
 import QuillEditor from "../component/QuillEditor";
 import ScrapUI from "../component/ScrapUI";
-import { Row, Col, Space } from 'antd';
-import { Menu } from 'antd';
+import { Row, Col, Space } from "antd";
+import { Menu } from "antd";
+// import { useParams } from "react-router-dom";
 
 function Editor() {
-  const state = {
-    current: 'mail',
-  };
+  //   let { lectureId } = useParams();
 
-  const handleClick = e => {
-    console.log('click ', e);
+  const handleClick = (e) => {
+    console.log("click ", e);
     this.setState({ current: e.key });
   };
   return (
-    <div style={{ marginLeft: '3rem',marginRight: '3rem',marginTop: '5rem'}}>
+    <div style={{ marginLeft: "3rem", marginRight: "3rem", marginTop: "5rem" }}>
       <Row justify="center" align="top" gutter={[32, 16]}>
-      <Col span={12}><QuillEditor /></Col>
-      <Col span={12}><ScrapUI /></Col>
-    </Row>
-    <div style={{ marginTop: '1rem'}}>
-    <Menu onClick={handleClick}  mode="horizontal">
-        <Menu.Item key="mail" >
-          Mind Maps
-        </Menu.Item>
-        <Menu.Item key="mail" >
-          Flowcharts
-        </Menu.Item>
-        <Menu.Item key="mail" >
-          Render
-        </Menu.Item>
-        <Menu.Item key="mail" >
-          PDF 
-        </Menu.Item>
-      </Menu>
-    </div>
-    <iframe id="theFrame" src="https://zh.1lib.in/book/2079252/21276b" width='100%' height='600' >
-    </iframe>
+        <Col span={12}>
+          <QuillEditor />
+        </Col>
+        <Col span={12}>
+          <ScrapUI />
+        </Col>
+      </Row>
+      <div style={{ marginTop: "1rem" }}>
+        <Menu onClick={handleClick} mode="horizontal">
+          <Menu.Item key="mail">Mind Maps</Menu.Item>
+          <Menu.Item key="mail">Flowcharts</Menu.Item>
+          <Menu.Item key="mail">Render</Menu.Item>
+          <Menu.Item key="mail">PDF</Menu.Item>
+        </Menu>
+      </div>
+      <iframe
+        id="theFrame"
+        src="https://zh.1lib.in/book/2079252/21276b"
+        width="100%"
+        height="600"
+      ></iframe>
     </div>
   );
 }
