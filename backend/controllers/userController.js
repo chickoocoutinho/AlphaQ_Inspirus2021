856fd0f07da1bbd41b495b62ef2cdb35c03c2ac1
Deborah Mendes <deb.mendes.9@gmail.com>
2021-10-30 01:52:30 +0530

    fixed bug

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ const User = require("../models/user_model");
 module.exports.signup = async (req,res) => {
     try{
         const {name, email, imageUrl, googleId} = req.body;
-        const user = await User.indOneAndUpdate({email},{name,email,imageUrl,googleId},{upsert: true});
+        const user = await User.findOneAndUpdate({email},{name,email,imageUrl,googleId},{upsert: true});
         res.status(200).json({user_id: user._id})
     }
     catch(e){
