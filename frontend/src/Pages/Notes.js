19e4f80fe680e7be6e22e0481444e4196eeacafc
Deborah Mendes <deb.mendes.9@gmail.com>
2021-10-30 06:11:48 +0530

    Complete notes pages

--- frontend/src/Pages/Notes.js
+++ frontend/src/Pages/Notes.js
@@ -9,6 +9,7 @@ const { Search } = Input;
 const { Meta } = Card;
 
 const searchClient = algoliasearch(process.env.REACT_APP_ALGOLIA_APP_ID,process.env.REACT_APP_ALGOLIA_SEARCH_KEY)
+const myImgLinks = ["https://i.ibb.co/Ry9V0tR/anamnesis33-pj-X8-VSNo2-Ts-unsplash.jpg", "https://i.ibb.co/RjD1S2Z/faraz-arshad-hj-Ydr-Ujo7-JY-unsplash.jpg", "https://i.ibb.co/pZvz41G/ospan-ali-V-77-Fxf-Ge-QU-unsplash.jpg","https://i.ibb.co/hmSWWrp/patrick-fore-M1-Vo1-MFMv-XI-unsplash.jpg","https://i.ibb.co/xSyVptB/raimond-klavins-Afm-By-Tk-HK-Q-unsplash.jpg","https://i.ibb.co/PM2Xxp9/raimond-klavins-Ql6-Jh-Gdb-Qg0-unsplash.jpg","https://i.ibb.co/RQnm2j1/ransel-yoho-arkg-Tj3-XJq-A-unsplash.jpg","https://i.ibb.co/bvvvx8h/s-o-c-i-a-l-c-u-t-g-REi-9t-I5-Mg-unsplash.jpg" ]; 
 
 const Notes = () => {
     const history = useHistory()
@@ -33,15 +34,17 @@ const Notes = () => {
       console.log(notes)
 
     return (
-        <div style={{ maxWidth: '60%', marginLeft:"24rem"}}>
-           <Search size="large" style={{width:"100%", marginTop: '50px', marginBottom:'50px'}} placeholder="input search text" onSearch={onSearch} onChange={e => onSearch(e.target.value)}/>
-           <Row gutter={[24, 16]} >
+        <div style={{ maxWidth: '60%', marginLeft:"26rem"}}>
+            <Search size="large" style={{width:"100%", marginTop: '50px', marginBottom:'50px'}} placeholder="input search text" onSearch={onSearch} onChange={e => onSearch(e.target.value)}/>
+            <div style={{marginTop: '2rem', marginLeft: '10%'}}>
+            
+           <Row gutter={[32, 16]} >
             {notes.length > 0 ? (
           notes.map((element) => (
             <Col span={8}>
             <Card key={element.id} hoverable
             style={{ width: 240, marginBottom: "2rem" }}
-            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+            cover={<img alt="example" src={myImgLinks[Math.floor(Math.random() * myImgLinks.length)]} />}
             onClick={() => redirectLecture(element.lecture_name,element.lecture_id,element.course_id)}>
               {element.lecture_name}
             </Card>
@@ -85,6 +88,7 @@ const Notes = () => {
             </>
         )}
         </Row>
+            </div>
         </div>
     )
 }
